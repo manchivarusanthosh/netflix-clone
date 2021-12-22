3156e6d685bf37c77ef552467e285cf4dff53c59
manchivarusanthosh <manchivarusanthoshkumar@gmail.com>
2021-12-22 21:24:02 +0530

    frontend completed ..navbar pending

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import "./App.css";
+import Banner from "./components/Banner/Banner";
+import Row from "./components/Row/Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <p>Netflix Clone</p>
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
---
+++ src/components/Row/Row.js
@@ -0,0 +1,41 @@
+// import axios from "axios";
+import React, { useState, useEffect } from "react";
+import instance from "../../axios";
+import "./row.css";
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await instance.get(fetchUrl);
+      setMovies(request.data.results);
+
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  // console.log("here the movies arrived", movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
